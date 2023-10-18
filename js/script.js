bcfd650bb48d6c1738e6998629ee90d1f01a5424
matteobonelli <matteobonelli08@gmail.com>
2023-10-18 15:17:07 +0200

    added gridFieldGenerator function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', function(){
-    const minedGrid = document.getElementById('minedGrid');
-    minedGrid.innerHTML = '';
-    squareNum = 100;
+    const playingField = document.getElementById('playingField');
+    playingField.innerHTML = '';
+    playingField.append(gridFieldGenerator());
+    // console.log(playingField)
+});
+
+function gridFieldGenerator(){
+    const minedGrid = document.createElement('div');
+    minedGrid.classList.add('minedGrid');
+    let squareNum = 100;
     for (let i = 0; i < squareNum; i++){
         let box = boxGenerator(i);
         // console.log(box);
         minedGrid.append(box);
     }
-});
+    return minedGrid
+}
 
 function boxGenerator(squareIndex){
     const square = document.createElement('div');
