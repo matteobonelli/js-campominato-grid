2e6372bc8e28db80f87102fecdf4090453d71dee
matteobonelli <matteobonelli08@gmail.com>
2023-10-18 14:58:56 +0200

    function boxGenerator created

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function(){
+    const minedGrid = document.getElementById('minedGrid');
+    squareNum = 100;
+    for (let i = 0; i < squareNum; i++){
+        let box = boxGenerator(i);
+        console.log(box);
+    }
+});
+
+function boxGenerator(squareIndex){
+    const square = document.createElement('div');
+    square.classList.add('box');
+    square.addEventListener('click', function(){
+        square.classList.add('active-box');
+        console.log(squareIndex + 1);
+    });
+    return square
+};
