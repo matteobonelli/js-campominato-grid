266d44b51e092d3165c6428e5a9149861fcfa915
matteobonelli <matteobonelli08@gmail.com>
2023-10-18 15:25:11 +0200

    bonus repo created

---
+++ bonus/js/script.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function(){
+    const playingField = document.getElementById('playingField');
+    playingField.innerHTML = '';
+    playingField.append(gridFieldGenerator());
+    // console.log(playingField)
+});
+
+function gridFieldGenerator(){
+    const minedGrid = document.createElement('div');
+    minedGrid.classList.add('minedGrid');
+    let squareNum = 100;
+    for (let i = 0; i < squareNum; i++){
+        let box = boxGenerator(i);
+        // console.log(box);
+        minedGrid.append(box);
+    }
+    return minedGrid
+}
+
+function boxGenerator(squareIndex){
+    const square = document.createElement('div');
+    square.classList.add('box');
+    square.addEventListener('click', function(){
+        square.classList.add('active-box');
+        console.log(squareIndex + 1);
+    });
+    return square
+};
