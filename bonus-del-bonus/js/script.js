1e51442f386ee03aadbc3f4e68750073ee29c14b
matteobonelli <matteobonelli08@gmail.com>
2023-10-18 19:14:40 +0200

    if you go on the wrong tile... boom!

---
+++ bonus-del-bonus/js/script.js
@@ -0,0 +1,79 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click',() => {
+    const selectDifficulty = document.getElementById('difficulty').value;
+    const playingField = document.getElementById('playingField');
+    let randomContainer = [];
+    if(selectDifficulty === ''){
+        return;
+    } else{
+        console.clear();
+        playingField.innerHTML = '';
+        const squareNum = difficultySelector(selectDifficulty);
+        for(let x = 0; x < 10; x++){
+            randomContainer.push(getRandomIntInclusive(1, squareNum))
+        }
+        console.log(randomContainer);
+        // console.log(squareNum);
+        playingField.append(gridFieldGenerator(squareNum, randomContainer));
+        // console.log(playingField)
+    }
+});
+
+function difficultySelector(selectDifficulty){
+    let squareNum;
+    if (selectDifficulty === 'easy'){
+        squareNum = 100;
+    } else if (selectDifficulty === 'medium'){
+        squareNum = 81;
+    } else if (selectDifficulty === 'hard'){
+        squareNum = 49;
+    }
+    return squareNum
+}
+
+function gridFieldGenerator(squareNumber, randomContainer){
+    const minedGrid = document.createElement('div');
+    minedGrid.classList.add('minedGrid');
+    for (let i = 0; i < squareNumber; i++){
+        let box = boxGenerator(i, squareNumber, randomContainer);
+        // console.log(box);
+        minedGrid.append(box);
+    }
+    return minedGrid
+}
+
+function boxGenerator(squareIndex, squareNumber, randomContainer){
+    const square = document.createElement('div');
+    const squareDimension = Math.sqrt(squareNumber);
+    square.style.width = `calc(100% / ${squareDimension})`;
+    square.style.height = square.style.width;
+    square.classList.add('box');
+    let boxAction = squareIndex + 1;;
+    let boxColor = 'active-box';
+    for(let x = 0; x < randomContainer.length; x++){
+        if(squareIndex + 1 === randomContainer[x]){
+             boxColor = 'boom-box';
+             boxAction = 'BOOM!';
+         };
+    }
+    square.innerHTML = squareIndex + 1; 
+    square.addEventListener('click', () => {
+        square.innerHTML = boxAction;
+        square.classList.add(boxColor);
+        if (boxColor === 'boom-box'){
+            console.log('Hai perso!');
+            
+        } else{
+            console.log(squareIndex + 1);
+        }
+    }, {once : true});
+    return square
+};
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+  }
+  
